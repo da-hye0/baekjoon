e475d925e1a963c32acb19013801ea3257f78d94
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2025-09-24 11:38:19 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -0,0 +1,16 @@
+import Foundation
+var dp = [Int](repeating: 0, count: 11)
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in 4...10 {
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+}
+
+let T = Int(readLine()!)!
+for _ in 0..<T {
+    let n = Int(readLine()!)!
+    print(dp[n])
+}
