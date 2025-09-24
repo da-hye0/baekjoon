c0ea19ef0b4791dbbae3b603dbe67449604876ff
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2025-09-24 11:02:46 +0900

    [Gold IV] Title: 동전 1, Time: 20 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.swift
+++ 백준/Gold/2293. 동전 1/동전 1.swift
@@ -14,6 +14,9 @@ dp[0] = 1
 for coin in coins {
     if coin > k { continue }
     for j in coin...k {
+        // Int64보다 값이 클 수 있어서 오버플로우는 건너뛰고 계산함.
+        // "경우의 수는 231보다 작다."라는 문구가 명시되어 있음.
+        // 결과적으로 계산중에 오버플로우를 허용하더라도 답은 Int값 안에 들어옴
         dp[j] = dp[j] &+ dp[j - coin]
     }
 }
