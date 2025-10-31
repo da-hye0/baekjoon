9bcf9210c4849f1705692b8e027629c1086512c2
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2025-10-31 15:13:52 +0900

    [Silver III] Title: 바이러스, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.swift
+++ 백준/Silver/2606. 바이러스/바이러스.swift
@@ -4,39 +4,22 @@ let N = Int(readLine()!)!
 let M = Int(readLine()!)!
 
 var graph = Array(repeating: [Int](), count: N + 1)
-
 for _ in 0..<M {
     let edge = readLine()!.split(separator: " ").map { Int($0)! }
-    let a = edge[0]
-    let b = edge[1]
-    
-    graph[a].append(b)
-    graph[b].append(a)
+    graph[edge[0]].append(edge[1])
+    graph[edge[1]].append(edge[0])
 }
 
 var visited = Array(repeating: false, count: N + 1)
 var infectedCount = 0
-
-func dfs(node: Int) {
-    visited[node] = true
-    for nextNode in graph[node] {
-        if !visited[nextNode] {
-            infectedCount += 1
-            dfs(node: nextNode)
-        }
-    }
-}
-
-dfs(node: 1)
 var queue = [1]
 visited[1] = true
-
 while !queue.isEmpty {
     let currentNode = queue.removeFirst()
     for nextNode in graph[currentNode] {
         if !visited[nextNode] {
             visited[nextNode] = true
-            infectedCount += 1
+            infectedCount += 1 
             queue.append(nextNode)
         }
     }
