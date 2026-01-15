4c9950465f7015392ba0025401e753db50eca7d4
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2026-01-15 10:47:09 +0900

    [Silver III] Title: 2×n 타일링, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+if n == 1{
+    print(1)
+} else  {
+    var dp = Array(repeating: 0, count: n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    
+    if n >= 3 {
+        for i in 3...n {
+            dp[i] = (dp[i-1] + dp [i-2]) % 10007
+        }
+    }
+    print(dp[n])
+}
